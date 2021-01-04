03da26492c5eaa206474838dead6349e27cae938
Walid Azzimani <walidazzimani@gmail.com>
2021-01-04 18:51:47 +0100

    fix form

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,7 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
-
-        ;
+            ->add('content', TextType::class, ['label' => false,]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
